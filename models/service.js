8f49fe8480233fa078173ca7f6c3d5cfbabd6c29
Bobby-Lishez <right2bob@msn.com>
2018-05-17 17:47:29 -0600

    started database/api routes

---
+++ models/service.js
@@ -0,0 +1,26 @@
+module.exports = function(sequelize, DataTypes) {
+    var Service = sequelize.define("Service", {
+        serviceType: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        date: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        url: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    Service.associate = function(models) {
+        Service.belongsTo(models.Car, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Service;
+};
--- server.js
+++ server.js
@@ -12,6 +12,9 @@ var app = express();
 //static content(public folder)
 app.use(express.static(path.join(__dirname, '/public')));
 
+//models for syncing
+var db = require("./models");
+
 //parsing
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -21,6 +24,8 @@ var apiRoutes = require("./controllers/api-routes.js");
 var htmlRoutes = require("./controllers/html-routes");
 
 //listener
+db.sequelize.sync({}).then(function() {
 app.listen(PORT, function(){
     console.log("App listening on http://localhost:" + PORT);
+});
 });
