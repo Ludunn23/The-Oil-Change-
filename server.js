325cc085d15fe6e6a31f2dfc4a64e18eccfc5184
Jessie <33649090+JessieHaus@users.noreply.github.com>
2018-05-22 18:46:24 -0600

    Bob (#23)
    * Meant to push this on Sunday Accidentally pushed to my liri repo instead
    
    * added info panel
    
    * Added Service Menu
    
    * Added login functionality to service menu, faqs, and myvehicles
    
    * fixed routing
    
    * started database/api routes
    
    * api routes functional, admin page added to manage database
    
    * fixed index page
    
    * Service history!
    
    * added faqs page

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use("/", htmlRoutes);
 
 
 //listener
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync({}).then(function() {
 app.listen(PORT, function(){
     console.log("App listening on http://localhost:" + PORT);
 });
