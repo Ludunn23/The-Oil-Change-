b9d504a4804e4bce3d2b6d3974f1117665eadd84
Bobby-Lishez <right2bob@msn.com>
2018-05-22 19:54:56 -0600

    jaws db

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports = {
+    "development": {
+      "username": process.env.JAWSDB_USERNAME,
+      "password": process.env.JAWSDB_PASSWORD,
+      "database": process.env.JAWSDB_DATABASE,
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "test": {
+      "username": "root",
+      "password": null,
+      "database": "database_test",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "production": {
+      "username": "root",
+      "password": null,
+      "database": "database_production",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    }
+  }
+  
