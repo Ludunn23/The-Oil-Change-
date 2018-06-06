9915ab2d82804b017b8e49f37f1aa5a92a1d84c4
Bobby-Lishez <33477821+Bobby-Lishez@users.noreply.github.com>
2018-06-05 20:57:13 -0600

    commented out priveleged admin route

--- controllers/auth-routes.js
+++ controllers/auth-routes.js
@@ -10,7 +10,7 @@ module.exports = function(app, passport) {
 
     app.get('/logout',authController.logout);
 
-    app.get('/admin', isLoggedIn, authController.admin);
+    //app.get('/admin', isLoggedIn, authController.admin);
 
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect: '/dashboard',
